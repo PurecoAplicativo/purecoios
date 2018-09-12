06b8474ed25c4eb13372bd2af1b8045e89ddcce0
Julianny Favinha <julianny.favinha@gmail.com>
2018-09-12 14:39:31 -0300

    Initial project

---
+++ Pureco/Pureco/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pureco
+//
+//  Created by Julianny Favinha on 9/12/18.
+//  Copyright © 2018 Pureco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
