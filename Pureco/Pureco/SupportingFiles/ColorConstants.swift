59a0a0bceafbecff5f62100af280ecc9e79a4ba0
Giovani Pereira <giovani.x.pereira@gmail.com>
2018-09-22 18:30:41 -0300

    Constants

---
+++ Pureco/Pureco/SupportingFiles/ColorConstants.swift
@@ -0,0 +1,17 @@
+//
+//  ColorConstants.swift
+//  Pureco
+//
+//  Created by Giovani Pereira on 22/09/18.
+//  Copyright © 2018 Pureco. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var purecoDarkBlue = UIColor(red: 25.0/255.0, green: 91.0/255.0, blue: 163.0/255.0, alpha: 1)
+    static var purecoMediumBlue = UIColor(red: 39.0/255.0, green: 142.0/255.0, blue: 203.0/255.0, alpha: 1)
+    static var purecoLightBlue = UIColor(red: 49.0/255.0, green: 169.0/255.0, blue: 221.0/255.0, alpha: 1)
+    
+}
