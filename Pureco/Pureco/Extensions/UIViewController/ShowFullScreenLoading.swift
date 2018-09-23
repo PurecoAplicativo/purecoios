ceac88d4c9d1584d289fc9a9abc9e12af6e3cbda
Giovani Pereira <giovani.x.pereira@gmail.com>
2018-09-22 21:19:02 -0300

    Added loading to screen

--- Pureco/Pureco/Extensions/UIViewController/ShowFullScreenLoading.swift
+++ Pureco/Pureco/Extensions/UIViewController/ShowFullScreenLoading.swift
@@ -10,18 +10,28 @@ import UIKit
 
 extension UIViewController {
     
-    func showFullScreenLoading() -> UIView {
-        
-        let view = UIView(frame: self.view.frame)
-        view.backgroundColor = UIColor.gray.withAlphaComponent(0.5)
+    var activityIndicatorLargeWhite: UIView {
         let activity = UIActivityIndicatorView(frame: .zero)
-        
-        self.view.addSubview(view)
-        
         activity.startAnimating()
-        view.addSubview(activity)
         activity.center = view.center
         activity.activityIndicatorViewStyle = .whiteLarge
+        return activity
+    }
+    
+    var purecoActivity: UIView {
+        let purecoActivity = PurecoLoaderImageView(image: #imageLiteral(resourceName: "loading_pureco10"))
+        purecoActivity.center = view.center
+        purecoActivity.animationDuration = 1.87
+        return purecoActivity
+    }
+    
+    func showFullScreenLoading(purecoStyle: Bool = true) -> UIView {
+        
+        let view = UIVisualEffectView(frame: self.view.frame)
+        view.effect = UIBlurEffect(style: .light)
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.contentView.addSubview(purecoStyle ? purecoActivity : activityIndicatorLargeWhite )
+        self.view.addSubview(view)
         
         return view
         
